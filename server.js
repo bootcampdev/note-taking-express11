9d4abfea18fbb6fbfa2185442acf0030b6baf944
kihBootCamp <kimberleyheuer@hotmail.com>
2021-01-28 21:55:38 -0600

    move files server.js up to main dir

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const path = require("path");
 const fs = require("fs");
 const express = require("express");
+const { json } = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const html_path = path.join(__dirname, "../../");
-const db_file = path.join(__dirname, "../../../db/db.json");
+const html_path = path.join(__dirname, "../public");
+const db_file = path.join(__dirname, "../develop/db/db.json");
 
-console.log("html path" , html_path);
-console.log("db path" , db_file);
-console.log(path.join(__dirname, "../../index.html"));
+console.log("html path: " , html_path);
+console.log("db path: " , db_file);
+console.log(path.join(__dirname, "/index.html"));
 
 // middleware
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
+
+//
+
+const myNotes = [
+    {
+        title: "title1",
+        text: "text1"
+    }
+];
 
 // html routes
 //
@@ -38,11 +48,24 @@ app.get("/api/notes", (req, res) => {
     fs.readFile(db_file, "ascii", (err, data) => {
         if (err) throw err;
         console.log("data is ", data);
+
+        var notes = JSON.parse(data);
+
+         notes.forEach(element => {
+            console.log(element.title);
+            console.log(element.text);            
+        });
+
+
     } )
 
-    res.send('success');
-    //res(data);
+    //console.log(notes);
+    //res.send('success');
+    //res.json(notes);
+    res.json(myNotes);
 })
+
+//app.post
 
 // start the listener
 //
