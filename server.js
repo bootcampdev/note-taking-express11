ab3c37fd6aa4eacd219962a48f5147c084151a62
kihBootCamp <kimberleyheuer@hotmail.com>
2021-01-30 10:22:20 -0600

    change read file to Synch

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const html_path = path.join(__dirname, "../public");
+const html_path = path.join(__dirname, "../develop/public");
 const db_file = path.join(__dirname, "../develop/db/db.json");
 
-console.log("html path: " , html_path);
-console.log("db path: " , db_file);
+console.log("html path: ", html_path);
+console.log("db path: ", db_file);
 console.log(path.join(__dirname, "/index.html"));
 
 // middleware
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 //
@@ -32,40 +32,86 @@ const myNotes = [
 app.get("/", (req, res) => {
     console.log(req.url);
     //res.send('success');
-    res.sendFile( path.join(html_path, "/index.html"));
+    res.sendFile(path.join(html_path, "/index.html"));
 })
 
 app.get("/notes", (req, res) => {
     console.log(req.url);
     //res.send('success');
-    res.sendFile( path.join(html_path, "/notes.html"));
+    res.sendFile(path.join(html_path, "/notes.html"));
 })
 
 // api calls
+// get all notes and send back
 //
 app.get("/api/notes", (req, res) => {
+    //
+    // read file will be string, readFile
 
-    fs.readFile(db_file, "ascii", (err, data) => {
-        if (err) throw err;
-        console.log("data is ", data);
+    // fs.readFile(db_file, "ascii", (err, data) => {
+    //     if (err) throw err;
+    //     console.log("data is ", data);
+
+    //     var notes = JSON.parse(data);
+
+    //      notes.forEach(element => {
+    //         console.log(element.title);
+    //         console.log(element.text);  
+
+    //         myNotes.push(element);
+
+    //         console.log("element", element);            
+    //     });
+
+    //     console.log("after push myNotes: ",myNotes);
+    //     res.json(myNotes);
+    //     //res.send(JSON.stringify(notes));
+    // } )
+
+    try {
+        const data = fs.readFileSync(db_file, { encoding: "ascii", flag: "r" });
 
         var notes = JSON.parse(data);
 
-         notes.forEach(element => {
+        notes.forEach(element => {
             console.log(element.title);
-            console.log(element.text);            
+            console.log(element.text);
+
+            myNotes.push(element);
+
+            console.log("element", element);
         });
 
+        console.log("after push myNotes: ", myNotes);
+        res.json(myNotes);
+    } catch (err) {
+        console.error(err);
+    }
 
-    } )
-
-    //console.log(notes);
-    //res.send('success');
-    //res.json(notes);
-    res.json(myNotes);
+    console.log("before return myNotes: ", myNotes);
+    //res.json(myNotes);
 })
 
-//app.post
+// add a new note
+//
+app.post("/api/notes", (req, res) => {
+
+    const newNote = req.body;
+    console.log(newNote);
+
+    myNotes.push(newNote);
+
+    fs.writeFile(db_file, JSON.stringify(myNotes), (err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send();
+        }
+    })
+
+    console.log(myNotes);
+    res.status(200).send();
+})
+
 
 // start the listener
 //
